5dc119433c93f6d14f24cc5417178279917c0955
ggkarikolrajbcs28-png <ggkarikolraj_bcs28@mepcoeng.ac.in>
2026-04-27 12:06:27 +0530

    Create RemoveDuplicates.py

---
+++ RemoveDuplicates.py
@@ -0,0 +1,10 @@
+n=int(input("enter the no of students:"))
+for i in range(n):
+   m=int(input("enter the marks of the students:"))
+   l=list()
+   l.append[m]
+s=set(l)
+l1=list(s)
+print("Original Marks:",l)
+print("The marks without dublicates:",l1)
+l1.sort()
